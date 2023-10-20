bc4b88ee49c95ff8e6d74cb64216f7420472b024
Habibur Rahman <habib.swi16@gmail.com>
2023-10-20 21:54:48 +0600

    toggle light and dark mode

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,11 +1,23 @@
 import { Toaster } from "react-hot-toast";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import { useEffect, useState } from "react";
 
 const Root = () => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "light");
+
+  useEffect(
+    function () {
+      localStorage.setItem("theme", theme);
+      const localTheme = localStorage.getItem("theme");
+      document.querySelector("html").setAttribute("data-theme", localTheme);
+    },
+    [theme]
+  );
+
   return (
     <div>
-      <Navbar />
+      <Navbar setTheme={setTheme} />
       <Outlet />
       <Toaster />
     </div>
