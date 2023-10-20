dcf504a4de46c2142d7d90882685b9c62c1e4600
Habibur Rahman <habib.swi16@gmail.com>
2023-10-21 00:15:14 +0600

    change footer design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="relative overflow-hidden py-10">
+    <footer className="relative overflow-hidden py-10 light-mode:bg-white dark-mode:bg-gray-800 light-mode:text-gray-900 dark-mode:text-gray-100">
       <div className="relative z-10 mx-auto max-w-7xl px-4">
         <div className="-m-6 flex flex-wrap">
           <div className="w-full p-6 md:w-1/2 lg:w-5/12">
@@ -14,11 +14,11 @@ const Footer = () => {
                 <span className="ml-4 text-lg font-bold">DigitalNest</span>
               </div>
               <div>
-                <p className="mb-4  text-base font-medium">
+                <p className="mb-4 text-base font-medium">
                   Elevate Your Digital Experience and Fuel Your Life with
                   Innovation
                 </p>
-                <p className="text-sm text-gray-600">
+                <p className="text-sm">
                   &copy; {new Date().getFullYear()}. All Rights Reserved by
                   DigitalNest.
                 </p>
@@ -27,72 +27,56 @@ const Footer = () => {
           </div>
           <div className="w-full p-6 md:w-1/2 lg:w-2/12">
             <div className="h-full">
-              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
+              <h3 className="tracking-px mb-9 text-xs font-semibold uppercase text-gray-500">
                 Shop
               </h3>
               <ul>
                 <li className="mb-4">
-                  <a className=" text-base font-medium text-gray-900 hover:text-gray-700">
-                    Products
-                  </a>
+                  <a className="text-base font-medium">Products</a>
                 </li>
                 <li className="mb-4">
-                  <a className=" text-base font-medium text-gray-900 hover:text-gray-700">
-                    Brands
-                  </a>
+                  <a className="text-base font-medium">Brands</a>
                 </li>
                 <li className="mb-4">
-                  <a className=" text-base font-medium text-gray-900 hover:text-gray-700">
-                    Deals
-                  </a>
+                  <a className="text-base font-medium">Deals</a>
                 </li>
               </ul>
             </div>
           </div>
           <div className="w-full p-6 md:w-1/2 lg:w-2/12">
             <div className="h-full">
-              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
+              <h3 className="tracking-px mb-9 text-xs font-semibold uppercase text-gray-500">
                 Support
               </h3>
               <ul>
                 <li className="mb-4">
-                  <a className=" text-base font-medium text-gray-900 hover:text-gray-700">
-                    Contact Us
-                  </a>
+                  <a className="text-base font-medium">Contact Us</a>
                 </li>
                 <li className="mb-4">
-                  <a className=" text-base font-medium text-gray-900 hover:text-gray-700">
-                    FAQ
-                  </a>
+                  <a className="text-base font-medium">FAQ</a>
                 </li>
                 <li>
-                  <a className=" text-base font-medium text-gray-900 hover:text-gray-700">
-                    Customer Support
-                  </a>
+                  <a className="text-base font-medium">Customer Support</a>
                 </li>
               </ul>
             </div>
           </div>
           <div className="w-full p-6 md:w-1/2 lg:w-3/12">
             <div className="h-full">
-              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
+              <h3 className="tracking-px mb-9 text-xs font-semibold uppercase text-gray-500">
                 Legal
               </h3>
               <ul>
                 <li className="mb-4">
-                  <a className=" text-base font-medium text-gray-900 hover:text-gray-700">
+                  <a className="text-base font-medium">
                     Terms &amp; Conditions
                   </a>
                 </li>
                 <li className="mb-4">
-                  <a className=" text-base font-medium text-gray-900 hover:text-gray-700">
-                    Privacy Policy
-                  </a>
+                  <a className="text-base font-medium">Privacy Policy</a>
                 </li>
                 <li>
-                  <a className=" text-base font-medium text-gray-900 hover:text-gray-700">
-                    Shipping Information
-                  </a>
+                  <a className="text-base font-medium">Shipping Information</a>
                 </li>
               </ul>
             </div>
